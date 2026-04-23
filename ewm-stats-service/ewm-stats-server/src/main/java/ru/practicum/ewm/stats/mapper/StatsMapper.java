75888ae58363382c2fc31ae764ae09039ed0bc51
viktoralex2000 <viktoralex2000@gmail.com>
2026-04-23 12:22:02 +0400

    diplom-1 ver 0.18

--- ewm-stats-service/ewm-stats-server/src/main/java/ru/practicum/ewm/stats/mapper/StatsMapper.java
+++ ewm-stats-service/ewm-stats-server/src/main/java/ru/practicum/ewm/stats/mapper/StatsMapper.java
@@ -19,44 +19,4 @@ public class StatsMapper {
         hit.setCreated(LocalDateTime.parse(dto.getTimestamp(), FORMATTER));
         return hit;
     }
-
-    /*public static EndpointHitDto toEndpointHitDto(EndpointHit hit) {
-        EndpointHitDto dto = new EndpointHitDto();
-
-        dto.setId(hit.getId());
-        dto.setApp(hit.getApp());
-        dto.setUri(hit.getUri());
-        dto.setIp(hit.getIp());
-        dto.setTimestamp(hit.getCreated());
-
-        return dto;
-    }*/
 }
-
-
-/*package ru.practicum.ewm.stats.mapper;
-
-import ru.practicum.ewm.stats.dto.EndpointHitDto;
-import ru.practicum.ewm.stats.model.EndpointHit;
-
-public class StatsMapper {
-
-    public static EndpointHit toEndpointHit(EndpointHitDto endpointHitDto) {
-        EndpointHit endpointHit = new EndpointHit();
-        endpointHit.setApp(endpointHitDto.getApp());
-        endpointHit.setUri(endpointHitDto.getUri());
-        endpointHit.setIp(endpointHitDto.getIp());
-        endpointHit.setCreated(endpointHitDto.getTimestamp());
-        return endpointHit;
-    }
-
-    public static EndpointHitDto toEndpointHitDto(EndpointHit endpointHit) {
-        EndpointHitDto endpointHitDto = new EndpointHitDto();
-        endpointHitDto.setId(endpointHit.getId());
-        endpointHitDto.setApp(endpointHit.getApp());
-        endpointHitDto.setUri(endpointHit.getUri());
-        endpointHitDto.setIp(endpointHit.getIp());
-        endpointHitDto.setTimestamp(endpointHit.getCreated());
-        return endpointHitDto;
-    }
-}*/
