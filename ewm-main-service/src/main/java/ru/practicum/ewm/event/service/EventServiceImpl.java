781c6c41dab4ada72f3447ec98aac6768745d28b
viktoralex2000 <viktoralex2000@gmail.com>
2026-04-29 15:49:38 +0400

    diplom 2 ver 0.14

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.event.service;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
