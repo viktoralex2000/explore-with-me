f048164640352c7ff90be30050e759acf57b1620
viktoralex2000 <viktoralex2000@gmail.com>
2026-04-29 17:16:30 +0400

    diplom 2 ver 0.17

--- ewm-main-service/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -79,6 +80,15 @@ public class ErrorHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
                 "error", e.getMessage(),
                 "status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "timestamp", LocalDateTime.now()
+        ));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String, Object>> handleMissingParam(MissingServletRequestParameterException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                "error", "Required parameter is missing: " + e.getParameterName(),
+                "status", HttpStatus.BAD_REQUEST.value(),
                 "timestamp", LocalDateTime.now()
         ));
     }
