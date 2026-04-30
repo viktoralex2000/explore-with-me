0cb466ad6901a874c1118a16fb0bf2b544e67e41
viktoralex2000 <viktoralex2000@gmail.com>
2026-04-30 04:04:34 +0400

    diplom 2 ver 1.1

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.event.model.Event;
 import ru.practicum.ewm.event.model.EventState;
 
@@ -15,21 +16,21 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findAllByInitiatorId(Long userId, Pageable pageable);
 
     @Query("""
-            select e from Event e
-            where e.state = :state
-              and (:text is null or (lower(e.annotation) like lower(concat('%', :text, '%'))
-                    or lower(e.description) like lower(concat('%', :text, '%'))))
-              and (:categories is null or e.category.id in :categories)
-              and (:paid is null or e.paid = :paid)
-              and (e.eventDate >= :rangeStart)
-              and (e.eventDate <= :rangeEnd)
-            """)
-    Page<Event> searchPublic(EventState state,
-                             String text,
-                             List<Long> categories,
-                             Boolean paid,
-                             LocalDateTime rangeStart,
-                             LocalDateTime rangeEnd,
+        select e from Event e
+        where e.state = :state
+          and (:text is null or (lower(e.annotation) like lower(concat('%', :text, '%'))
+                or lower(e.description) like lower(concat('%', :text, '%'))))
+          and (:categories is null or e.category.id in :categories)
+          and (:paid is null or e.paid = :paid)
+          and (e.eventDate >= :rangeStart)
+          and (e.eventDate <= :rangeEnd)
+        """)
+    Page<Event> searchPublic(@Param("state") EventState state,
+                             @Param("text") String text,
+                             @Param("categories") List<Long> categories,
+                             @Param("paid") Boolean paid,
+                             @Param("rangeStart") LocalDateTime rangeStart,
+                             @Param("rangeEnd") LocalDateTime rangeEnd,
                              Pageable pageable);
 
     @Query("""
@@ -40,10 +41,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
               and (e.eventDate >= :rangeStart)
               and (e.eventDate <= :rangeEnd)
             """)
-    Page<Event> searchAdmin(List<Long> users,
-                            List<EventState> states,
-                            List<Long> categories,
-                            LocalDateTime rangeStart,
-                            LocalDateTime rangeEnd,
+    Page<Event> searchAdmin(@Param("users") List<Long> users,
+                            @Param("states") List<EventState> states,
+                            @Param("categories") List<Long> categories,
+                            @Param("rangeStart") LocalDateTime rangeStart,
+                            @Param("rangeEnd") LocalDateTime rangeEnd,
                             Pageable pageable);
 }
